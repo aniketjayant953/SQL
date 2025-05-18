6b6290ca03591a33200730e26efad3a9e5e0559e
aniketjayant953 <aniketjayant953@gmail.com>
2025-05-18 21:17:23 +0530

    add data

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -19,3 +19,67 @@ CALL place_order(3, 3, '6,7', @total);
 SELECT @total;
 SELECT * FROM orders;
 SELECT * FROM order_details;
+
+
+-- ### hello_world stored procedure
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `hello_world`()
+-- BEGIN
+-- 	SELECT "Hello world";
+-- END
+
+
+-- ### add_user stored procedure
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `add_user`(IN input_name VARCHAR(255), IN input_email VARCHAR(255), OUT message VARCHAR(233))
+-- BEGIN
+
+-- 	-- chech if input_email exist in user table
+--     DECLARE user_count INTEGER;
+--     SELECT COUNT(*) INTO user_count FROM users WHERE email = input_email;
+-- 	
+--     -- insert the new user
+--     IF user_count = 0 THEN
+-- 		INSERT INTO users (name,email) VALUES (input_name, input_email);
+-- 		SET message = 'user inserted';
+-- 	ELSE
+-- 		SET message = 'user already exist';
+--     END IF;
+-- 	
+
+-- END
+
+-- ### user_details stored procedure
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `users_details`(IN input_email VARCHAR(255))
+-- BEGIN
+-- 	DECLARE id INTEGER;
+--     SELECT user_id INTO id FROM users WHERE email = input_email;
+--     
+--     SELECT * FROM orders WHERE user_id = id;
+-- END
+
+
+-- ### place_order stored procedure
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `place_order`(IN input_user_id INT, IN input_r_id INT, IN input_f_ids VARCHAR(20), OUT total_amount INT)
+-- BEGIN
+-- 	
+--     -- insert into orders table
+--     DECLARE new_order_id INT;
+--     DECLARE f_id1 INT;
+--     DECLARE f_id2 INT;
+--     
+--     SET f_id1 = SUBSTRING_INDEX(input_f_ids, ',', 1);
+--     SET f_id2 = SUBSTRING_INDEX(input_f_ids, ',', -1);
+--     
+--     SELECT MAX(order_id) + 1 INTO new_order_id FROM orders;
+--     
+--     SELECT SUM(price) INTO total_amount FROM menu
+--     WHERE r_id = input_r_id AND f_id IN (f_id1, f_id2);
+--     
+--     INSERT INTO orders (order_id, user_id, r_id, amount, date) VALUES
+--     (new_order_id, input_user_id, input_r_id, total_amount, DATE(NOW()));
+
+-- 	-- insert into orders_details table
+--     INSERT INTO order_details (order_id, f_id) VALUES (new_order_id, f_id1), (new_order_id, f_id2);
+--     
+
+-- END
+
--- userdefined_functions.sql
+++ userdefined_functions.sql
@@ -19,3 +19,55 @@ SELECT flights_between('Bangaluru','New Delhi') as num_flights FROM flights;
 
 SELECT COUNT(*) FROM flights
 	WHERE source = 'Bangaluru' AND destination = 'New Delhi';
+    
+    
+    
+-- ### Make these Functions to run the above queries
+
+-- ### hello_world function
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `hello_world`() RETURNS varchar(255) CHARSET utf8mb4
+-- BEGIN
+
+-- RETURN 'Hello World';
+-- END
+
+-- ### age function
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `age`(dob VARCHAR(255)) RETURNS int
+-- BEGIN
+--     SET dob = STR_TO_DATE(dob, '%d-%m-%Y');
+-- RETURN (DATEDIFF(DATE(NOW()),dob))/365;
+-- END
+
+-- ### proper_name function
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `proper_name`(name VARCHAR(255), gender VARCHAR(233), married VARCHAR(255) ) RETURNS varchar(255) CHARSET utf8mb4
+-- BEGIN
+-- 	DECLARE title VARCHAR(244);
+--     SET name = CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2)));
+--     IF gender = 'M' THEN
+-- 		SET title = CONCAT('MR',' ',name);
+-- 	ELSE
+-- 		IF married = 'Y' THEN
+-- 			SET title = CONCAT('Mrs',' ',name);
+-- 		ELSE 
+-- 			SET title = CONCAT('Ms',' ',name);
+-- 		END IF;
+-- 	END IF;
+-- RETURN title;
+-- END
+
+-- ### format_date function
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `format_date`(doj VARCHAR(255)) RETURNS varchar(255) CHARSET utf8mb4
+-- BEGIN
+-- 	SET doj = STR_TO_DATE(doj, '%d-%m-%Y');
+-- RETURN DATE_FORMAT(doj,'%D %b %y');
+-- END
+
+-- ### flights_between function
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `flights_between`(city1 VARCHAR(233), city2 VARCHAR(233)) RETURNS int
+-- BEGIN
+-- 	
+-- RETURN (
+-- 	SELECT COUNT(*) FROM flights
+-- 	WHERE source = city1 AND destination = city2
+-- );
+-- END
